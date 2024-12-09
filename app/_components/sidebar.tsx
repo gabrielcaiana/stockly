763bf6823aeca52276862c094c39ec6417faf28a
gabriel.caiana <gabrielcaianaguedes@gmail.com>
2024-12-09 00:21:50 -0300

    feat: :recycle: adiciona configurações do VSCode para excluir a pasta node_modules

--- app/_components/sidebar.tsx
+++ app/_components/sidebar.tsx
@@ -1,17 +1,31 @@
+import { LayoutGrid, PackageIcon, ShoppingBasketIcon } from "lucide-react";
+import SidebarButton from "./sidebar-button";
+
 const Sidebar = () => {
   return (
     <div className="w-64 bg-white">
       <div className="px-8 py-6">
-        <h1 className="font-bold text-2xl">Stockly</h1>
+        <h1 className="text-2xl font-bold">Stockly</h1>
       </div>
 
       <div className="flex flex-col gap-2 p-2">
-        <button className="px-6 py-3">Dashboard</button>
-        <button className="px-6 py-3">Produtos</button>
-        <button className="px-6 py-3">Vendas</button>
+        <SidebarButton href="/">
+          <LayoutGrid size={20} />
+          Dashboard
+        </SidebarButton>
+
+        <SidebarButton href="products">
+          <PackageIcon size={20} />
+          Produtos
+        </SidebarButton>
+
+        <SidebarButton href="/sales">
+          <ShoppingBasketIcon size={20} />
+          Vendas
+        </SidebarButton>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
