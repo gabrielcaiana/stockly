fbc1112ea798a8332c0060a43413c3be2f0abb1d
gabrielcaiana <gabrielcaianaguedes@gmail.com>
2024-12-14 11:52:41 -0300

    feat: :sparkles: implementa listagem de produtos com botão para adicionar novo produto

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,5 +1,31 @@
-const Products = () => {
-  return <div>Products</div>
-}
+import { PlusIcon } from "lucide-react";
+import { db } from "../_lib/prisma";
+import { Button } from "../_components/ui/button";
+import { DataTable } from "../_components/ui/data-table";
+import { productTableColumns } from "./_components/table-columns";
 
-export default Products
+const Products = async () => {
+  const products = await db.product.findMany();
+
+  return (
+    <div className="w-full space-y-8 bg-white p-8">
+      <div className="flex w-full items-center justify-between">
+        <div className="space-y-1">
+          <span className="text-xs font-semibold text-slate-500">
+            Gestão de produtos
+          </span>
+          <h2 className="text-xl font-semibold">Produtos</h2>
+        </div>
+
+        <Button className="gap-2">
+          <PlusIcon size={20} />
+          Novo produto
+        </Button>
+      </div>
+
+      <DataTable columns={productTableColumns} data={products} />
+    </div>
+  );
+};
+
+export default Products;
