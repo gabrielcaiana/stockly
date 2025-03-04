4b1ae002520f2d996de61046252fb283d16af68b
gabrielcaiana <gabrielcaianaguedes@gmail.com>
2025-03-04 18:29:09 -0300

    feat: refactor product actions into a dedicated dropdown menu component for improved code organization

--- app/products/_components/table-columns.tsx
+++ app/products/_components/table-columns.tsx
@@ -1,36 +1,10 @@
 "use client";
 
-import { useState } from "react";
-
-import {
-  AlertDialog,
-  AlertDialogTrigger,
-} from "@/app/_components/ui/alert-dialog";
 import { Badge } from "@/app/_components/ui/badge";
-import { Button } from "@/app/_components/ui/button";
-
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger,
-} from "@/app/_components/ui/dropdown-menu";
 
 import { Product } from "@prisma/client";
 import { ColumnDef } from "@tanstack/react-table";
-import {
-  ClipboardCopyIcon,
-  EditIcon,
-  MoreHorizontalIcon,
-  TrashIcon,
-} from "lucide-react";
-
-import { toast } from "sonner";
-import DeleteProductDialogContent from "./delete-dialog-content";
-import { Dialog, DialogTrigger } from "@/app/_components/ui/dialog";
-import UpsertProductDialogDialog from "./upsert-dialog-content";
+import TableDropdownMenu from "./table-dropdown-menu";
 
 const inStock = "IN_STOCK";
 
@@ -76,62 +50,7 @@ export const productTableColumns: ColumnDef<Product & { status: string }>[] = [
     accessorKey: "actions",
     header: "Ações",
     cell: function ActionCell(row) {
-      const [editDialogIsOpen, setEditDialogIsOpen] = useState(false);
-
-      const product = row.row.original;
-
-      const copyId = () => {
-        navigator.clipboard.writeText(product.id);
-        toast.success("ID copiado com sucesso!");
-      };
-
-      return (
-        <AlertDialog>
-          <Dialog open={editDialogIsOpen} onOpenChange={setEditDialogIsOpen}>
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <Button variant="ghost">
-                  <MoreHorizontalIcon size={16} />
-                </Button>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent>
-                <DropdownMenuLabel>Ações</DropdownMenuLabel>
-                <DropdownMenuSeparator />
-
-                <DropdownMenuItem className="gap-1.5" onClick={copyId}>
-                  <ClipboardCopyIcon size={16} />
-                  Copiar ID
-                </DropdownMenuItem>
-
-                <DialogTrigger asChild>
-                  <DropdownMenuItem className="gap-1.5">
-                    <EditIcon size={16} />
-                    Editar
-                  </DropdownMenuItem>
-                </DialogTrigger>
-
-                <AlertDialogTrigger className="flex gap-1.5">
-                  <DropdownMenuItem className="gap-1.5">
-                    <TrashIcon size={16} />
-                    Excluir
-                  </DropdownMenuItem>
-                </AlertDialogTrigger>
-              </DropdownMenuContent>
-            </DropdownMenu>
-
-            <UpsertProductDialogDialog defaultValues={{
-              id: product.id,
-              name: product.name,
-              price: Number(product.price),
-              stock: product.stock,
-            }}
-            onSuccess={() => setEditDialogIsOpen(false)}
-            />
-
-            <DeleteProductDialogContent product={product} />
-          </Dialog>
-        </AlertDialog>
-      );
+      return <TableDropdownMenu product={row.row.original} />
     },
   },
 ];
---
+++ app/products/_components/table-dropdown-menu.tsx
@@ -0,0 +1,95 @@
+"use client"
+
+import { Button } from "@/app/_components/ui/button";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/app/_components/ui/dropdown-menu";
+
+import {
+  AlertDialog,
+  AlertDialogTrigger,
+} from "@/app/_components/ui/alert-dialog";
+
+import {
+  ClipboardCopyIcon,
+  EditIcon,
+  MoreHorizontalIcon,
+  TrashIcon,
+} from "lucide-react";
+
+import DeleteProductDialogContent from "./delete-dialog-content";
+import { Dialog, DialogTrigger } from "@/app/_components/ui/dialog";
+import UpsertProductDialogDialog from "./upsert-dialog-content";
+import { useState } from "react";
+import { Product } from "@prisma/client";
+import { toast } from "sonner";
+
+interface TableDropdownMenuProps {
+  product: Product
+}
+
+const TableDropdownMenu = ({ product }: TableDropdownMenuProps) => {
+  const [editDialogIsOpen, setEditDialogIsOpen] = useState(false);
+
+  const copyId = () => {
+    navigator.clipboard.writeText(product.id);
+    toast.success("ID copiado com sucesso!");
+  };
+
+  return (
+    <AlertDialog>
+      <Dialog open={editDialogIsOpen} onOpenChange={setEditDialogIsOpen}>
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost">
+              <MoreHorizontalIcon size={16} />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>Ações</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+
+            <DropdownMenuItem className="gap-1.5" onClick={copyId}>
+              <ClipboardCopyIcon size={16} />
+              Copiar ID
+            </DropdownMenuItem>
+
+            <DialogTrigger asChild>
+              <DropdownMenuItem className="gap-1.5">
+                <EditIcon size={16} />
+                Editar
+              </DropdownMenuItem>
+            </DialogTrigger>
+
+            <AlertDialogTrigger className="flex gap-1.5">
+              <DropdownMenuItem className="gap-1.5">
+                <TrashIcon size={16} />
+                Excluir
+              </DropdownMenuItem>
+            </AlertDialogTrigger>
+          </DropdownMenuContent>
+        </DropdownMenu>
+
+        <UpsertProductDialogDialog
+          defaultValues={{
+            id: product.id,
+            name: product.name,
+            price: Number(product.price),
+            stock: product.stock,
+          }}
+          onSuccess={() => setEditDialogIsOpen(false)}
+        />
+
+        <DeleteProductDialogContent product={product} />
+      </Dialog>
+    </AlertDialog>
+  );
+};
+
+export default TableDropdownMenu;
