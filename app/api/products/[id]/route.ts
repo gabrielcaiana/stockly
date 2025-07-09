b3e92618c1a802547d0f1cf9e4a90cf911aa263f
gabrielcaiana <gabrielcaianaguedes@gmail.com>
2025-07-09 17:08:38 -0300

    fix: update product API routes to handle errors and improve response structure

--- app/api/products/[id]/route.ts
+++ app/api/products/[id]/route.ts
@@ -4,34 +4,44 @@ import { db } from "@/app/_lib/prisma";
 
 export async function GET(
   request: Request,
-  { params }: { params: { id: string } }
+  { params }: { params: { id: string } },
 ) {
-  const productId = params.id;
+  try {
+    const productId = params.id;
 
-  const product = await db.product.findUnique({
-    where: {
-      id: productId
+    const product = await db.product.findUnique({
+      where: {
+        id: productId,
+      },
+    });
+
+    if (!product) {
+      return Response.json({ message: "Product not found" }, { status: 404 });
     }
-  })
 
-  if(!product) {
-    return Response.json({ message: 'Product not found' }, { status: 404 });
+    return Response.json(product, { status: 200 });
+  } catch (error) {
+    console.error("Database error:", error);
+    return Response.json({ message: "Internal server error" }, { status: 500 });
   }
-
-  return Response.json(product, { status: 200 });
 }
 
 export async function DELETE(
   request: Request,
-  { params }: { params: { id: string } }
+  { params }: { params: { id: string } },
 ) {
-  const productId = params.id;
+  try {
+    const productId = params.id;
 
-  await db.product.delete({
-    where: {
-      id: productId
-    }
-  })
+    await db.product.delete({
+      where: {
+        id: productId,
+      },
+    });
 
-  return Response.json({}, { status: 200, statusText: 'Product deleted' });
+    return Response.json({}, { status: 200, statusText: "Product deleted" });
+  } catch (error) {
+    console.error("Database error:", error);
+    return Response.json({ message: "Internal server error" }, { status: 500 });
+  }
 }
